632441e962768f0b7ee077336997ea8a039bc8b3
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2021-02-03 17:26:28 +0100

    ini update to sdk6

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -3,13 +3,10 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNMappPluginPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactlibrary.RNMappPluginModule;
-import com.reactlibrary.RNMappPluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,9 +22,8 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
-                    new MainReactPackage(),
-            new RNMappPluginPackage(),
-                    new RNMappPluginPackage()
+                    new MainReactPackage()
+
             );
         }
 
